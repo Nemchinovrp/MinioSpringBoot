2e1dee26af58acbeb1fa77125a118b67d0f86d45
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-12-23 09:09:56 +0300

    main

---
+++ src/main/java/com/minio/nemchinovrp/NemchinovrpApplication.java
@@ -0,0 +1,13 @@
+package com.minio.nemchinovrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemchinovrpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NemchinovrpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minio/nemchinovrp/NemchinovrpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minio.nemchinovrp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemchinovrpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
